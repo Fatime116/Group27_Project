e6cb8731768c95123da0f046b77c6c8b94b129a2
Fatime116 <patigul116@gmail.com>
2020-11-01 13:01:57 -0800

    Fatime added more comments on TC_Fatime

--- src/test/java/TC_Fatime.java
+++ src/test/java/TC_Fatime.java
@@ -3,5 +3,7 @@ public class TC_Fatime {
     public static void main(String[] args) {
 
         System.out.println("Girls are so powerfullll!!!!");
+        //comment
+        //more comment
     }
 }
